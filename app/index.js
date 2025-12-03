b1925d0353dd26b2a1e6764934f0ab9f796a5c8b
jugcf <juliagalindo030604@gmail.com>
2025-12-03 14:33:08 -0300

    update do layout

--- app/index.js
+++ app/index.js
@@ -1,44 +1,14 @@
-//CODIGO PARA BAIXAR PLANILHA
-document.addEventListener("DOMContentLoaded", () => {
-  const planilhas = {
-    "CPO-D": {
-      "TOTAL": "planilhas/Total-cpo-d.xlsx",
-      "COMPONENTE": "planilhas/PComp-cpo-d.xlsx",
-      "CARIADO":"planilhas/PComp-cpo-d.xlsx",
-      "OBTURADO":"planilhas/PComp-cpo-d.xlsx",
-      "PERDIDO":"planilhas/PComp-cpo-d.xlsx"
-    },
-    "ceo-d": {
-      "TOTAL": "planilhas/Total-ceo-d.xlsx",
-      "COMPONENTE": "planilhas/PComp-ceo-d.xlsx",
-      "CARIADO":"planilhas/PComp-ceo-d.xlsx",
-      "OBTURADO":"planilhas/PComp-ceo-d.xlsx",
-      "PERDIDO":"planilhas/PComp-ceo-d.xlsx"
-    }
-  };
+function downloadBtn(){
+//TODO - implementar a lógica do download da planilha 
+console.log("teste downloadBtn");
+}
 
-  const downloadBtn = document.getElementById("downloadBtn");
-  const indexSelect = document.getElementById("indexSelect");
-  const distSelect = document.getElementById("distSelect");
+function selectFileBtn(){
+//TODO - implementar a lógica do download dos arquivos 
+console.log("teste selectFileBtn");
+}
 
-  if (downloadBtn) {
-    downloadBtn.addEventListener("click", () => {
-      const indice = indexSelect.value;
-      const distribuicao = distSelect.value;
-
-      const caminhoArquivo = planilhas[indice]?.[distribuicao];
-
-      if (caminhoArquivo) {
-        const link = document.createElement("a");
-        link.href = caminhoArquivo;
-        link.download = caminhoArquivo.split("/").pop();
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      } else {
-        alert("Planilha não encontrada para esta combinação de Índice e Distribuição.");
-      }
-    });
-  }
-});
-//FIM CODIGO BAIXAR PLANILHA
+function generateBtn(){
+//TODO - implementar a lógica do download dos arquivos 
+console.log("teste generateBtn");
+}
